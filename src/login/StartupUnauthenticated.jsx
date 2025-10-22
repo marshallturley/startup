a9ecda8269582af94726376d8e4ac6afa09d0336
Marshall Turley <mturley0@byu.edu>
2025-10-21 23:25:59 -0600

    Update login.jsx and create StartupUnauthorized.jsx

---
+++ src/login/StartupUnauthenticated.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { MessageDialog } from './messaggeDialog';
+
+export function StartupUnauthenticated({ onLogin }) {
+        const [username, setUsername] = React.useState(propTypes.userName);
+        const [password, setPassword] = React.useState('');
+        const [displayError, setDisplayError] = React.useState(null);
+
+        async function loginUser() {
+            localStorage.setItem('userName', userName);
+            propTypes.onLogin(userName);
+        }
+
+        async function createUser() {
+            localStorage.setItem('userName', userName);
+            propTypes.onLogin(userName)
+        }
+
+        return (
+            <main className="py-2">
+            <h2 className="text-center">Please Login or Register Here</h2>
+            <form className="text-center">
+                <div>
+                    <input type="text" id="username" placeholder="Username" className="inputs my-2" onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div>
+                    <input type="password" id="password" placeholder="Password" className="inputs my-2" onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button onClick={() => loginUser()} className="btn btn-outline-dark m-2">Login</button>
+                <button onClick={() => createUser()} className="btn btn-outline-dark m-2">Register</button>
+            </form>
+            <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+        </main>
+        )
+    }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { StartupUnauthenticated } from './StartupUnauthenticated';
+import { StartupAuthenticated } from './StartupAuthenticated';
+import {AuthState } from './AuthState';
 
-export function Login() {
+export function Login({userName, authState, onAuthChange}) {
   return (
-    <main className="py-2">
-            <h2 className="text-center">Please Login or Register Here</h2>
-            <form className="text-center">
-                <div>
-                    <input type="text" id="username" placeholder="Username" className="inputs my-2"/>
-                </div>
-                <div>
-                    <input type="password" id="password" placeholder="Password" className="inputs my-2"/>
-                </div>
-                <button type="button" onclick="alert('Login functionality placeholder')" className="btn btn-outline-dark m-2">Login</button>
-                <button type="button" onclick="alert('Register functionality placeholder')" className="btn btn-outline-dark m-2">Register</button>
-            </form>
-        </main>
+    <main>
+        {authState === AuthState.Authenticated && (
+            <StartupAuthenticated userName={userName} onLogout={() => onAuthChange('', AuthState.Unauthenticated)} />
+        
+        )}
+        {authState === AuthState.Unauthenticated && (
+            <StartupUnauthenticated onLogin={(loginUserName) => onAuthChange(loginUserName, AuthState.Authenticated)} />
+        )}
+    </main>
   );
 }
