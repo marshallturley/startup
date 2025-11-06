d87f1b04f2492c0b18a1ee72391ee771ff968b52
Marshall Turley <mturley0@byu.edu>
2025-11-06 15:28:20 -0700

    Add 3rd party weather API

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -7,7 +7,7 @@ export function Dashboard() {
 
     const [userName, setUserName] = useState('');
     const [totalWorkouts, setTotalWorkouts] =useState(0);
-    const [weather, setWeather] = useState({ temperature: '', conditions: ''});
+    const [weather, setWeather] = useState({ temperature: '', precipitation: ''});
 
     useEffect(() => {
         const storedUser = localStorage.getItem('userName');
@@ -17,8 +17,14 @@ export function Dashboard() {
     }, []);
 
     useEffect(() => {
-        const mockWeather = { temperature: '72F', conditions: 'Sunny' };
-        setWeather(mockWeather)
+        fetch('https://api.open-meteo.com/v1/forecast?latitude=40.2338&longitude=-111.6585&hourly=temperature_2m,precipitation&timezone=America%2FDenver&forecast_days=3&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch')
+            .then(res => res.json())
+            .then(data => {
+                setWeather({
+                    temperature: data.hourly.temperature_2m[0],
+                    precipitation: data.hourly.precipitation[0]
+                });
+            });
     }, []);
 
     useEffect(() => {
@@ -41,7 +47,8 @@ export function Dashboard() {
         <h2 className="mt-3 mb-5"><b>Welcome, {userName}</b></h2>
         <section className="text-center mb-5">
             <h3>Current Weather</h3>
-            <div className="my-2">{weather.temperature}, {weather.conditions}</div>
+            <div className="my-2">Temperature: {weather.temperature}</div>
+            <div className="my-2">Precipitation: {weather.precipitation}</div>
         </section>
         <section className="text-center mb-5">
             <h3 className="mt-2">{userName}'s Total Workouts Logged</h3>
